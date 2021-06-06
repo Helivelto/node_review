5f05f707184d4c6389c7c5ebb5ebbae59a720f52
Helivelto <helivelto.souza321@gmail.com>
2021-06-06 16:21:42 -0300

    juntando arquivos review.js e reviews.js

--- app/routes/reviews.js
+++ app/routes/reviews.js
@@ -7,4 +7,22 @@ module.exports = (application) => {
             res.render('reviews/reviews', { reviews: result})
         })
     })
+
+    application.get('/review', (req, res) => {
+        // res.render('reviews/review')
+
+    const connection = application.config.dbConnection;
+    const reviewDao = new application.app.models.ReviewsDAO(connection);
+        // reviewDao.getReviews((error, result) => {
+        //     res.render('reviews/review', {review: result})
+        // })
+        reviewDao.getReviews((error, result) => {
+            res.render("reviews/review", {review: result})
+        })
+    
+
+        connection.query('select * from noticias where id = 2', function(error, result){
+            res.render('reviews/review', {review: result})
+        })
+    })
 }
