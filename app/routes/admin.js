9eaa318f6796731a8b51bd7c9f34ae73f42bda8a
Helivelto <helivelto.souza321@gmail.com>
2021-06-06 16:43:23 -0300

    adicionando admin.js no controllers

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,16 +1,10 @@
 module.exports = (application) => {
     application.get('/add-new-review', (req, res) => {
-        res.render('admin/add_novos_reviews')
+        application.app.controllers.admin.add_new_review(application, req, res)
     })
 
     application.post('/new/reviews', (req, res) => {
-        const review = req.body;
-
-        const connection = application.config.dbConnection;
-        const reviewsDao = new application.app.models.ReviewsDAO(connection)
-        reviewsDao.salvarReview(review, (error, result) => {
-            res.redirect('/reviews')
-        })
+        application.app.controllers.admin.new_review(application, req, res);
     })
 }
 
