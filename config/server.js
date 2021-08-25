82547c153824012343a5df357cc5cfb20db33b54
Helivelto <helivelto.souza321@gmail.com>
2021-08-25 15:12:34 -0300

    install and config express-session

--- config/server.js
+++ config/server.js
@@ -2,17 +2,32 @@ const express = require('express')
 const consign = require('consign')
 const expressValidator = require('express-validator')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 // const expressValidator = require('express-validator')
 
+// Express
 let app = express()
 
+// Sessiom
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'my_secret_keyApp',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: true }
+}))
+
+
+// EJS
 app.set('view engine', 'ejs')
 app.set('views', './app/views')
 
+// Public
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(expressValidator())
 app.use(express.static('./app/public'))
 
+// Consign
 consign()
 .include('app/routes')
 .then('config/dbConnection.js')
