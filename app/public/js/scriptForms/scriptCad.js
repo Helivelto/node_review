6e524c930bbecd4c65d6cadc26499e1bce0f00a0
Helivelto <helivelto.souza321@gmail.com>
2021-12-16 15:05:30 -0300

    validation the  register form with validatorJS

--- app/public/js/scriptForms/scriptCad.js
+++ app/public/js/scriptForms/scriptCad.js
@@ -14,34 +14,37 @@ const nome_user = document.querySelector('#nome_usuario')
 go_btn.addEventListener('click', function(e){
     let  validacao = 0
 
-    if(nome_user.value == ''){
+    if(validator.isEmpty(nome_user.value)){
         nome_user.classList.add('border_red')
     }
     else{
-        // email.classList.add('border_green')
+        nome_user.classList.add('inputValid')
         validacao++
     }
 
 
-    if(email.value == ''){
+    if (validator.isEmail(email.value) == false || validator.isEmpty(email.value)) {
         email.classList.add('border_red')
     }
     else{
-        // email.classList.add('border_green')
+        email.classList.add('inputValid')
         validacao++
     }
+
 
     if(confirmPass.value == '' && pass.value == '' || pass.value != confirmPass.value){
         confirmPass.classList.add('border_red')
         pass.classList.add('border_red')
     } 
     else{
-        // confirmPass.classList.add('border_green')
-        // pass.classList.add('border_green')
+        pass.classList.add('inputValid')
+        confirmPass.classList.add('inputValid')
         validacao++
     }
 
+
     if(validacao != 3){
+        alert('Preencha as informações corretamente!')
         validacao = 0
     } else {
         pt1.style.display = 'none'
