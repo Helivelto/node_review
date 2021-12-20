781a99e7061a75b1ce352ca09044ae35a1bda8b3
Helivelto <helivelto.souza321@gmail.com>
2021-12-20 14:10:39 -0300

    stylizing the any profile page

--- app/controllers/perfilController.js
+++ app/controllers/perfilController.js
@@ -13,14 +13,21 @@ module.exports.anyperfil = (application, req, res) => {
     // const User = require('../models/User')
 
     // const usuario = User.findUsers()
-
+    const myUser = req.user
+    console.log(`My user is ${myUser.nome_usuario}`)
     const autor = req.query
     console.log(autor)
     const connection = application.config.dbConnection;
     const usuariosDao = new application.app.models.UsuariosDAO(connection)
 
     usuariosDao.getUser(autor, (error, result) => {
-        res.render('usuarios/anyProfile', { userProfile: result })
+
+        if (autor.autor == myUser.nome_usuario) {
+            res.redirect('/perfil')
+        } else {
+            res.render('usuarios/anyProfile', { userProfile: result })
+        }
+
         console.log(result)
     })
 
