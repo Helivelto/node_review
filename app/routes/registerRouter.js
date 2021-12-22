4d86ea7fb954453c76fcd2e574f04fe100726300
Helivelto <helivelto.souza321@gmail.com>
2021-12-22 15:52:35 -0300

    adding a fileFilter to images jpeg and png

--- app/routes/registerRouter.js
+++ app/routes/registerRouter.js
@@ -1,16 +1,31 @@
 module.exports = (application) => {
     const multer = require('multer')
 
+
+    const fileFilter = (req, file, cb) => {
+        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+            cb(null, true)
+        } else {
+            cb(null, false)
+            console.log("Isso n é uma img")
+        }
+    }
     const storage = multer.diskStorage({
         destination: function(req, file, cb){
             cb(null, "uploads/")
         },
         filename: function(req, file, cb){
-            cb(null, file.originalname)
+            cb(null, Date.now().toString() + "_" + file.originalname)
         }
     })
     
-    const upload = multer({storage})
+    const upload = multer({
+        storage: storage,
+        fileFilter: fileFilter,
+        limits: {
+            fileSize: 1024 * 1024 * 5,
+        }
+    })
 
     application.get('/cadastro', (req, res) => {
         application.app.controllers.registerController.form_cadastro(application, req, res)
