4acf6cf5ca8d6b8d4511d0489af32213a04d626c
Helivelto <helivelto.souza321@gmail.com>
2021-12-23 16:36:45 -0300

    if you have-not a photo, your photo is == null

--- app/controllers/registerController.js
+++ app/controllers/registerController.js
@@ -10,16 +10,23 @@ module.exports.cadastro_usuario = async (application, req, res) => {
     try {
 
         const hashedPassword = await bcrypt.hash(req.body.senha_usuario, 10)
-        const usuarios = ({
-            nome_usuario: req.body.nome_usuario,
-            email_usuario: req.body.email_usuario,
-            senha_usuario: hashedPassword,
-            foto_usuario: req.file.filename
-        })
+        let foto_usuario
+        if (req.file == undefined) {
+            foto_usuario = null
+        } else {
+            foto_usuario = req.file.filename
+        }
+
+        // const usuarios = ({
+        //     nome_usuario: req.body.nome_usuario,
+        //     email_usuario: req.body.email_usuario,
+        //     senha_usuario: hashedPassword,
+        //     foto_usuario: req.file.filename
+        // })
         // console.log(req.file.path)
         DbConnection.connection().query(`insert into usuarios(nome_usuario, email_usuario, senha_usuario, cod_status_usuario, foto_usuario) 
         values ('${req.body.nome_usuario}', 
-          '${req.body.email_usuario}', '${hashedPassword}', 'a', '${req.file.filename}' )`)
+          '${req.body.email_usuario}', '${hashedPassword}', 'a', '${foto_usuario}' )`)
         res.redirect('/login')
 
     } catch(e) {
